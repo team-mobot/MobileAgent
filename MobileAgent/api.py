ca87b2210bf15f86a6f02d177eac0a6f8b62b627
Frank Leon Rose <frankleonrose@users.noreply.github.com>
2024-07-18 17:59:31 -0400

    Better error reporting from inference_chat

--- MobileAgent/api.py
+++ MobileAgent/api.py
@@ -14,7 +14,7 @@ def inference_chat(chat, API_TOKEN):
     }
 
     data = {
-        "model": 'gpt-4-vision-preview',
+        "model": 'gpt-4-turbo',
         "messages": [],
         "max_tokens": 2048,
     }
@@ -26,9 +26,23 @@ def inference_chat(chat, API_TOKEN):
         try:
             res = requests.post(api_url, headers=headers, json=data)
             res = res.json()['choices'][0]['message']['content']
-        except:
-            print("Network Error:")
-            print(res)
+        except Exception as e:
+            print(f"API access exception: {e}")
+            try:
+                result_json = res.json()
+                print(result_json)
+                error_code = result_json['error']['code']
+
+                if error_code == 'invalid_image_format':
+                    raise Exception("Invalid image format.")
+                elif error_code == 'rate_limit_exceeded':
+                    raise Exception("Rate limit exceeded. Retry later.")
+                elif error_code == 'insufficient_quota':
+                    raise Exception("Insufficient quota. Add credits and retry.")
+                else:
+                    raise Exception(f"Error code: {error_code}.")
+            except:
+                raise Exception(f"Unknown API error {e}.")
         else:
             break
     
